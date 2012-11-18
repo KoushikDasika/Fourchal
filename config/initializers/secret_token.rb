58f990f8afd9f27e1a0ba56e301873c2a4000c9b
Koushik Dasika <kdasika@koushikdasika.com>
2012-11-18 16:11:59 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fourchal::Application.config.secret_token = '5c7cebafdd35d1649e161baa60dfb2961fbca7041dc8d98c61ae349dfc621d2c9c319d3cb3d2f0ba48f6486ebe336f54256ef257c01039df5ca35f6c8811bb41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fourchal::Application.config.session_store :cookie_store, key: '_fourchal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fourchal::Application.config.session_store :active_record_store
