309dd34c7bbe5b22cce7a4accb06c5c5720779ba
Koushik Dasika <kdasika@koushikdasika.com>
2013-01-01 16:38:37 -0500

    lots of stuff

--- spec/models/challenge_entry_spec.rb
+++ spec/models/challenge_entry_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ChallengeEntry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @challenge_entry = FactoryGirl.create(:challenge_entry)
+  end
+
+  it "should have a valid factory" do
+    @challenge_entry.should be_valid
+  end
 end
